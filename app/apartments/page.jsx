398fbd861956bcff97536b34e11eb471204d9fdd
phlst <hajdu2024@gmail.com>
2025-04-09 13:58:33 +0200

    added suspense

--- app/apartments/page.jsx
+++ app/apartments/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Apartments from "../components/Apartments";
 import ApartmentSidebar from "../components/ApartmentSidebar";
 
@@ -6,7 +7,9 @@ export default function ApartmentsPage({ searchParams }) {
     <div className="flex h-screen">
       <ApartmentSidebar />
       <div className="flex-1 overflow-y-auto">
-        <Apartments searchParams={searchParams} />
+        <Suspense fallback={<div>Loading apartments...</div>}>
+          <Apartments searchParams={searchParams} />
+        </Suspense>
       </div>
     </div>
   );
